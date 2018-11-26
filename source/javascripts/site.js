47c86be82cd856a46c03cf504c6649b054f4fc89
Josh Franklin <jfank7588@gmail.com>
2018-11-26 14:14:53 -0500

    wip

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,82 +1,84 @@
 // This is where it all goes :)
 
-// Accordion
-function adjustWidth(){
-  var parentwidth = $('#accordion').width();
-  $('.usa-accordion-header').width(parentwidth);
-}
-
-// Fix accordion header if scrolled
-
-$(window).scroll(function(){
-  var scrollTop = $(window).scrollTop();
-  var elementOffset = $('#accordion').offset().top;
-  var distance = (elementOffset - scrollTop);
-  console.log(distance);
-
-  if (distance <= 0) {
-    $('.usa-accordion-header').addClass('usa-accordion-header-fixed');
-    adjustWidth();
-  } else {
-    $('.usa-accordion-header').removeClass('usa-accordion-header-fixed');
+$(document).ready(function(){
+  // Accordion
+  function adjustWidth(){
+    var parentwidth = $('#accordion').width();
+    $('.usa-accordion-header').width(parentwidth);
   }
-});
 
-// Resize accordion headed if fixed to match accordion width
+  // Fix accordion header if scrolled
 
-$(window).resize (function(){
-  adjustWidth();
-})
+  $(window).scroll(function(){
+    var scrollTop = $(window).scrollTop();
+    var elementOffset = $('#accordion').offset().top;
+    var distance = (elementOffset - scrollTop);
+    console.log(distance);
 
-// Phone number block
-
-$("#dsn-phone-number-block").hide();
-
-$(function() {
-  $("#dns").click(function () {
-    if($(this).is(":checked")) {
-      $("#dsn-phone-number-block").show();
-      $("#phone-number-block").hide();
+    if (distance <= 0) {
+      $('.usa-accordion-header').addClass('usa-accordion-header-fixed');
+      adjustWidth();
     } else {
-      $("#dsn-phone-number-block").hide();
-      $("#phone-number-block").show();
+      $('.usa-accordion-header').removeClass('usa-accordion-header-fixed');
     }
+  });
+
+  // Resize accordion headed if fixed to match accordion width
+
+  $(window).resize (function(){
+    adjustWidth();
   })
-})
 
-$(function() {
-  $("#dont-know").click(function(){
-    if($(this).is(":checked")) {
-      $(".phone-input").attr("disabled", true);
-    } else {
-      $(".phone-input").removeAttr("disabled");
-    }
+  // Phone number block
+
+  $("#dsn-phone-number-block").hide();
+
+  $(function() {
+    $("#dns").click(function () {
+      if($(this).is(":checked")) {
+        $("#dsn-phone-number-block").show();
+        $("#standard-phone-number-block").hide();
+      } else {
+        $("#dsn-phone-number-block").hide();
+        $("#standard-phone-number-block").show();
+      }
+    })
   })
-})
 
-// Full address block
-
-$(".apo-fpo-dpo-address-block").hide();
-
-$(function() {
-  $("#apo-fpo-dpo-checkbox").click(function () {
-    if($(this).is(":checked")) {
-      $(".apo-fpo-dpo-address-block").show();
-      $(".address-block").hide();
-    } else {
-      $(".apo-fpo-dpo-address-block").hide();
-      $(".address-block").show();
-    }
+  $(function() {
+    $("#dont-know").click(function(){
+      if($(this).is(":checked")) {
+        $(".phone-input").attr("disabled", true);
+      } else {
+        $(".phone-input").removeAttr("disabled");
+      }
+    })
   })
-})
 
-// Date blocks
-$(function() {
-  $("#present").click(function(){
-    if($(this).is(":checked")) {
-      $(".to-date-input").attr("disabled", true);
-    } else {
-      $(".to-date-input").removeAttr("disabled");
-    }
+  // Full address block
+
+  $(".apo-fpo-dpo-address-block").hide();
+
+  $(function() {
+    $("#apo-fpo-dpo-checkbox").click(function () {
+      if($(this).is(":checked")) {
+        $(".apo-fpo-dpo-address-block").show();
+        $(".address-block").hide();
+      } else {
+        $(".apo-fpo-dpo-address-block").hide();
+        $(".address-block").show();
+      }
+    })
+  })
+
+  // Date blocks
+  $(function() {
+    $("#present").click(function(){
+      if($(this).is(":checked")) {
+        $(".to-date-input").attr("disabled", true);
+      } else {
+        $(".to-date-input").removeAttr("disabled");
+      }
+    })
   })
 })
